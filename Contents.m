e83968065d1a6ff60ce9c041926327d340b1306a
Marko Budisic <mbudisic@gmail.com>
2015-11-09 15:47:43 -0600

    Added Contents.m

---
+++ Contents.m
@@ -0,0 +1,15 @@
+% +CONTINUOUSFLOWS
+%
+% Files
+%   ABCFlow           - Unsteady ABC flow.
+%   BickleyRossby     - Polar jet with Rossby traveling wave perturbation.
+%   ContinuousFlow    - Abstract class specifying interface of a "continuous-time dynamical system".
+%   DoubleGyre        - Planar double-gyre system, as used by Shadden (2005)
+%   Duffing           - Unforced, undamped Duffing oscillator.
+%   FourGyre          - Planar four-gyre system, as used by Mezic (2010)
+%   HackbornRotOsc    - Hackborn Rotor-Oscillator flow -- Hackborn et al., JFM, (1997)
+%   Hamiltonian2DFlow - Abstract class specifying interface of a "continuous-time" Hamiltonian system in terms of its stream function Psi.
+%   Harmonic2D        - Simple linear harmonic oscillator. Energy function is x^2/2 + y^2/2
+%   lgwt              - Compute Legendre-Gauss points and weights.
+%   ODEFlow           - Abstract class specifying interface of a "continuous-time dynamical system".
+%   tumblerplot       - TUMBLERPLOT
