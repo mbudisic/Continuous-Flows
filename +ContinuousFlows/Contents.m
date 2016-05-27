7072a2ff450b9cd218d60b116e61d7b0982eb0ba
Marko Budisic <mbudisic@gmail.com>
2016-05-27 10:16:49 -0500

    Updated Contents files

--- +ContinuousFlows/Contents.m
+++ +ContinuousFlows/Contents.m
@@ -1,15 +1,22 @@
 % +CONTINUOUSFLOWS
 %
 % Files
-%   ABCFlow           - Unsteady ABC flow.
-%   BickleyRossby     - Polar jet with Rossby traveling wave perturbation.
-%   ContinuousFlow    - Abstract class specifying interface of a "continuous-time dynamical system".
-%   DoubleGyre        - Planar double-gyre system, as used by Shadden (2005)
-%   Duffing           - Unforced, undamped Duffing oscillator.
-%   FourGyre          - Planar four-gyre system, as used by Mezic (2010)
-%   HackbornRotOsc    - Hackborn Rotor-Oscillator flow -- Hackborn et al., JFM, (1997)
-%   Hamiltonian2DFlow - Abstract class specifying interface of a "continuous-time" Hamiltonian system in terms of its stream function Psi.
-%   Harmonic2D        - Simple linear harmonic oscillator. Energy function is x^2/2 + y^2/2
-%   lgwt              - Compute Legendre-Gauss points and weights.
-%   ODEFlow           - Abstract class specifying interface of a "continuous-time dynamical system".
-%   tumblerplot       - TUMBLERPLOT
+%   ABCFlow                   - Unsteady ABC flow.
+%   BickleyRossby             - Polar jet with Rossby traveling wave perturbation.
+
+%   DoubleGyre                - Planar double-gyre system, as used by Shadden (2005)
+%   Duffing                   - Undamped Duffing oscillator.
+%   FourGyre                  - Planar four-gyre system, as used by Mezic (2010)
+%   HackbornRotOsc            - Hackborn Rotor-Oscillator flow -- Hackborn et al., JFM, (1997)
+
+%   Harmonic2D                - Simple linear harmonic oscillator. Energy function is x^2/2 + y^2/2
+
+
+
+%   AbstractContinuousFlow    - CONTINUOUSFLOW Abstract class specifying interface of a "continuous-time dynamical system".
+%   AbstractHamiltonian2DFlow - HAMILTONIAN2DFLOW Abstract class specifying interface of a "continuous-time" Hamiltonian system in terms of its stream function Psi.
+%   AbstractODEFlow           - ODEFLOW Abstract class specifying interface of a "continuous-time dynamical system".
+%   AbstractODEFlow2D         - VELOCITYFIELD2D Abstract methods for 2D plots of velocity field.
+%   InterpolatedODEFlow2D     - Interpolated 2d velocity field, for example from PIV
+%   Vanderpol                 - Vanderpol oscillator.
+
---
+++ Contents.m
@@ -0,0 +1,4 @@
+% CONTINUOUSFLOWS
+%
+% Files
+
