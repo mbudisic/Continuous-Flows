f75697537d8b262eb9fb8fb63bc76cb15bedb399
Marko Budisic <marko@clarkson.edu>
2017-10-19 07:11:11 -0400

    Interpolated flows now have their own version of streamplot.

--- +ContinuousFlows/InterpolatedODEFlow2D.m
+++ +ContinuousFlows/InterpolatedODEFlow2D.m
@@ -17,6 +17,90 @@ classdef InterpolatedODEFlow2D < ContinuousFlows.AbstractODEFlow2D
 
   methods
 
+    function [varargout] = streamplot( obj, t, varargin)
+    %STREAM Level sets of the stream function of the flow.
+    %
+    % STREAMPLOT(obj, t)
+    %   Plots the streamlines of the interpolated flow starting from a
+    %   random subset of gridpoints. If t has multiple elements, video is produced.
+    % STREAMPLOT(obj,t, PARAMETER,VALUE )
+    %   Additionally modify the behavior using named parameters. Default
+    %   value in parenthesis
+    %   'R' -- number of points in vertical/horizontal grid (100)
+    %   'xi' -- manually set x grid; ignore if empty ([])
+    %   'yi' -- manually set y grid; ignore if empty ([])
+    %   'nlines' -- number of streamlines to compute (30)
+    %   'stepsize' -- stepsize for streamline calcluation (see built-in
+    %   STREAMLINE)
+    %   'npoints' -- number of points in each streamline (see built-in STREAMLINE)
+    %
+    % [h] = STREAMPLOT(...)
+    %   As above, returns graphics handle.
+    % [X,Y,U,V] = STREAMPLOT(...)
+    %   Returns spatial points and values of the velocity field.
+    %   U,V are matrices of size [rows(X), cols(X), numel(t)]
+    %
+    % See also: STREAMLINE
+
+      parser = inputParser;
+
+      %% Process optional parameters
+      parser.addParameter('R',100); % resolution of the grid
+      parser.addParameter('xi', [] ); % xgrid that should be used
+      parser.addParameter('yi', [] ); % ygrid that should be used
+      parser.addParameter('nlines',30, @(n)(n>0));
+      parser.addParameter('stepsize',0.1, @(x)(x>0 && x<1));
+      parser.addParameter('npoints',200, @(n)(n>0));
+
+      parser.parse(varargin{:});
+
+      R = ceil(parser.Results.R);
+      if isempty(parser.Results.xi)
+        xi = linspace(obj.Domain(1,1), obj.Domain(1,2), R);
+      else
+        xi = parser.Results.xi;
+      end
+      if isempty(parser.Results.yi)
+        yi = linspace(obj.Domain(2,1), obj.Domain(2,2), R);
+      else
+        yi = parser.Results.yi;
+      end
+
+      %% Compute 2D grid of velocities
+
+      [X,Y] = meshgrid(xi, yi);
+      x = [X(:),Y(:)].';
+      U = nan( [size(X), numel(t)] );
+      V = nan( [size(X), numel(t)] );
+      for k = 1:numel(t)
+        VF_i = obj.vf(t(k),x);
+        U(:,:,k) = reshape(VF_i(1,:), size(X));
+        V(:,:,k) = reshape(VF_i(2,:), size(X));
+      end
+
+      %% Use built-in streamline to construct streamlines
+
+      if nargout > 1
+        varargout = {X,Y,U, V};
+      else
+        sel = unique(randi(numel(X),ceil(parser.Results.nlines)));
+        for k = 1:numel(t)
+          cla;
+          h = streamline(X,Y,...
+                         U(:,:,k), V(:,:,k),...
+                         X(sel), Y(sel), ...
+                         [parser.Results.stepsize,...
+                          ceil(parser.Results.npoints)]);
+          title(sprintf('t = %.2f',t(k)));
+          pause(1/15);
+        end
+        if nargout > 0
+          varargout = {h};
+        end
+      end
+    end
+
+
     function obj = InterpolatedODEFlow2D( dt, ...
                                           axesNodes, ...
                                           UxGrid, UyGrid, ...
@@ -53,10 +137,10 @@ classdef InterpolatedODEFlow2D < ContinuousFlows.AbstractODEFlow2D
       %                   documentation for description of options (default:
       %                   'nearest')
       %
-      % 'test'          - plot the first slice of the quiver plot (with a 
-      %                   small grid offset) to visually compare input data  
-      %                   and interpolated data. 
-      %                   Passed value is the percentage of the grid step 
+      % 'test'          - plot the first slice of the quiver plot (with a
+      %                   small grid offset) to visually compare input data
+      %                   and interpolated data.
+      %                   Passed value is the percentage of the grid step
       %                   which is used to offset data
       %                   (default: 0)
       %
@@ -66,16 +150,16 @@ classdef InterpolatedODEFlow2D < ContinuousFlows.AbstractODEFlow2D
 
       obj.dt = dt;
       validateattributes( UxGrid, {'numeric'}, {}, ...
-          'InterpolatedODEFlow2D', 'UxGrid', 3 );
+                          'InterpolatedODEFlow2D', 'UxGrid', 3 );
       D = numel(size(UxGrid));
 
       validateattributes( axesNodes, {'cell'}, {'numel',D},...
-          'InterpolatedODEFlow2D', 'axesNodes', 2 );
-        
-      
+                          'InterpolatedODEFlow2D', 'axesNodes', 2 );
+
+
       validateattributes( UyGrid, {'numeric'}, {'ndims',D},...
-          'InterpolatedODEFlow2D', 'UyGrid', 4 );
-      
+                          'InterpolatedODEFlow2D', 'UyGrid', 4 );
+
       assert( all(size(UxGrid) == size(UyGrid)),...
               'Velocity components have to be of the same size' );
 
@@ -86,7 +170,7 @@ classdef InterpolatedODEFlow2D < ContinuousFlows.AbstractODEFlow2D
       parsearg.addParameter('interpolation','linear',@ischar);
       parsearg.addParameter('extrapolation','nearest',@ischar);
       parsearg.addParameter('test',0,@(x)(x >= 0));
-      
+
       parsearg.parse(varargin{:});
       params = parsearg.Results;
 
@@ -102,23 +186,23 @@ classdef InterpolatedODEFlow2D < ContinuousFlows.AbstractODEFlow2D
         obj.Domain(d,1) = min(axesNodes{d});
         obj.Domain(d,2) = max(axesNodes{d});
       end
-      
+
       gridCols = size(UxGrid,2);
       gridRows = size(UxGrid,1);
-      
+
       sizeX = numel(axesNodes{1});
       sizeY = numel(axesNodes{2});
       sizeT = numel(axesNodes{3});
-      
+
       assert( sizeX == gridRows, 'X-coordinate should correspond to rows' );
       assert( sizeY == gridCols, 'Y-coordinate should correspond to columns' );
       assert( all(size(UxGrid) == size(UyGrid)),...
-          'Dimensions of velocity matrices should match');
-      
+              'Dimensions of velocity matrices should match');
+
       if ~obj.isAutonomous
         assert( numel(axesNodes{3}) == size(UxGrid,3) );
       end
-      
+
       % interpolate the velocity field
       obj.Ux = griddedInterpolant(axesNodes, ...
                                   UxGrid,...
@@ -135,34 +219,34 @@ classdef InterpolatedODEFlow2D < ContinuousFlows.AbstractODEFlow2D
       obj.intprops = odeset(obj.intprops, 'Vectorized', 'on');
       %      obj.intprops = odeset(obj.intprops, 'Jacobian', @(t,x)obj.jacobian(t,x) );
       %      obj.intprops = odeset(obj.intprops, 'Stats','on' );
-      
+
       if params.test > 0
-          params.test = params.test/100;
-          x = axesNodes{1};
-          y = axesNodes{2};
-          t = axesNodes{3};
-          t0 = t(1);
-          ts = t0+params.test*diff(t(1:2));
-          
-          
-          [X,Y] = ndgrid(x,y);
-          
-          quiver(X,Y,UxGrid(:,:,1),UyGrid(:,:,1),'Color','r'); 
-          hold all; 
-          
-          h = obj.quiverplot(ts, 'grid', ...
-              {x+params.test*diff(x(1:2));...
-              y+params.test*diff(y(1:2));}); 
-          h.Color = 'b'; 
-          hold off;
-          
-          xlabel( sprintf('X - Range [%f,%f]', min(x), max(x) ) );
-          ylabel( sprintf('Y - Range [%f,%f]', min(y), max(y) ) );
-          title({sprintf('Input data (red) at t=%f',t0),...
-              sprintf('Interpolated data (blue) at t = %f',ts)});
-          
+        params.test = params.test/100;
+        x = axesNodes{1};
+        y = axesNodes{2};
+        t = axesNodes{3};
+        t0 = t(1);
+        ts = t0+params.test*diff(t(1:2));
+
+
+        [X,Y] = ndgrid(x,y);
+
+        quiver(X,Y,UxGrid(:,:,1),UyGrid(:,:,1),'Color','r');
+        hold all;
+
+        h = obj.quiverplot(ts, 'grid', ...
+                           {x+params.test*diff(x(1:2));...
+                            y+params.test*diff(y(1:2));});
+        h.Color = 'b';
+        hold off;
+
+        xlabel( sprintf('X - Range [%f,%f]', min(x), max(x) ) );
+        ylabel( sprintf('Y - Range [%f,%f]', min(y), max(y) ) );
+        title({sprintf('Input data (red) at t=%f',t0),...
+               sprintf('Interpolated data (blue) at t = %f',ts)});
+
       end
-      
+
 
     end
 
