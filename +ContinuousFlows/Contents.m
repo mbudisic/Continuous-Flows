5dd2ed8cd1b0e7e474b78eb6782fb75a5d18eddc
Marko Budisic <mbudisic@clarkson.edu>
2019-10-01 09:40:35 -0400

    Updated contents files

--- +ContinuousFlows/Contents.m
+++ +ContinuousFlows/Contents.m
@@ -1,29 +1,32 @@
 % +CONTINUOUSFLOWS
 %
 % Files
-%   ABCFlow                   - Unsteady ABC flow.
-%   BickleyRossby             - Polar jet with Rossby traveling wave perturbation.
+%   ABCFlow                      - Unsteady ABC flow.
+%   BickleyRossby                - Polar jet with Rossby traveling wave perturbation.
 
-%   DoubleGyre                - Planar double-gyre system, as used by Shadden (2005)
-%   Duffing                   - Undamped Duffing oscillator.
-%   FourGyre                  - Planar four-gyre system, as used by Mezic (2010)
-
-
-%   Harmonic2D                - Simple linear harmonic oscillator. Energy function is x^2/2 + y^2/2
+%   DoubleGyre                   - Planar double-gyre system, as used by Shadden (2005)
+%   Duffing                      - Undamped Duffing oscillator.
+%   FourGyre                     - Planar four-gyre system, as used by Mezic (2010)
 
 
+%   Harmonic2D                   - Simple linear harmonic oscillator. Energy function is x^2/2 + y^2/2
 
-%   AbstractContinuousFlow    - CONTINUOUSFLOW Abstract class specifying interface of a "continuous-time dynamical system".
-%   AbstractHamiltonian2DFlow - HAMILTONIAN2DFLOW Abstract class specifying interface of a "continuous-time" Hamiltonian system in terms of its stream function Psi.
-%   AbstractODEFlow           - ODEFLOW Abstract class specifying interface of a "continuous-time dynamical system".
-%   AbstractODEFlow2D         - Abstract methods for 2D plots of velocity field.
-%   InterpolatedODEFlow2D     - Interpolated 2d velocity field, for example from PIV
 
-%   InterpolatedODEFlow2D     - Interpolated 2d velocity field, for example from PIV
-%   LorenzExtended            - Lorenz system, as extended by Lyubimov, Zaks (Phys D, 1983)
-%   Vanderpol                 - Vanderpol oscillator.
+
+%   AbstractContinuousFlow       - CONTINUOUSFLOW Abstract class specifying interface of a "continuous-time dynamical system".
+%   AbstractHamiltonian2DFlow    - HAMILTONIAN2DFLOW Abstract class specifying interface of a "continuous-time" Hamiltonian system in terms of its stream function Psi.
+%   AbstractODEFlow              - ODEFLOW Abstract class specifying interface of a "continuous-time dynamical system".
+%   AbstractODEFlow2D            - Abstract methods for 2D plots of velocity field.
+%   InterpolatedODEFlow2D        - Interpolated 2d velocity field, for example from PIV
+
+%   InterpolatedODEFlow2D        - Interpolated 2d velocity field, for example from PIV
+%   LorenzExtended               - Lorenz system, as extended by Lyubimov, Zaks (Phys D, 1983)
+%   Vanderpol                    - Vanderpol oscillator.
 
 %   CherryFlow                   - Cherry Flow as described in Zaks (2001)
 %   CherryHamiltonianFlow        - Cherry Flow as described in Zaks (2001) for
 %   HackbornWeldonRotOsc         - Hackborn-Weldon Rotor-Oscillator flow -- Hackborn et
 %   HackbornWeldonRotOscVertical - HACKBORNROTOSCFROZEN Hackborn Rotor-Oscillator flow -- Hackborn et al., JFM, (1997)
+%   DMDdemoFlow                  - 
+%   DuffingDamped                - Damped Duffing Oscillator
+
--- Contents.m
+++ Contents.m
@@ -2,3 +2,4 @@
 %
 % Files
 
+%   demo - Using Continuous Flows
