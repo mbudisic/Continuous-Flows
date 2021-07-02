1578adbc7d384d2f42e20bd8922c087921251d1b
Marko <mbudisic@clarkson.edu>
2021-07-02 10:52:27 -0400

    Added sum->min biot savart

--- +ContinuousFlows/PointVortex.m
+++ +ContinuousFlows/PointVortex.m
@@ -10,6 +10,7 @@ classdef PointVortex < ContinuousFlows.AbstractODEFlow
         % vector of signed vorticities
         gamma(1,:) {mustBeNumeric, mustBeNonempty} = nan;
         core(1,1) {mustBePositive, mustBeNonempty} = 1e-6; % radius of core of the vector (no force)
+        type (1,1) logical = true; % if true - usual Biot-Savart, if false, replace sum by max.
     end
     
     properties (Dependent)
@@ -61,19 +62,29 @@ classdef PointVortex < ContinuousFlows.AbstractODEFlow
             sel = L < obj.core;
             L(sel) = -inf;
             
-            
+            % Compute the stream function            
+            Psi = -log(L).*obj.gamma/2/pi;
+            if ~obj.type
+                [Psix, idx] = min(Psi, [], 2);
+            else
+                Psix = sum(Psi, 2);
+                
+            end                
+            Psix = squeeze( Psix );
+                        
 
             % Now, Biot-Savart produces velocity vectors orthogonal to D
             V = ( cat(3, -D(:,:,2), D(:,:,1) ) )./(L.^2) ...
                 .* obj.gamma /2/pi;
             
-            
-            Vx = permute( sum( V, 2 ), [1,3,2] );
+            if obj.type
+                Vx = sum( V, 2 );
+            else
+                Vx = V(:,idx,:);
+            end
+            Vx = permute( Vx, [1,3,2] );
             vx = reshape( Vx, [], 1); % make into column
             
-            % Similarly, we can use the vortex 
-            Psi = -log(L).*obj.gamma/2/pi;            
-            Psix = squeeze( sum(Psi, 2) );
             
             
         end
